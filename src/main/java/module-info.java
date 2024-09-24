5d2926c2a0fced4b4722605b2d3ed7e4c4f4b3ef
saam <nospher4z@gmail.com>
2024-09-24 15:18:52 +0200

    Add pom / java depences

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,7 @@ module appli.todolistjx {
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens appli to javafx.fxml;
     exports appli;
