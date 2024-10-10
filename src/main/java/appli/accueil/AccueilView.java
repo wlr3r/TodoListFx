ee3b7c1381b8e4d70b6df45463088e995a7c9e3b
saam <nospher4z@gmail.com>
2024-10-10 15:33:48 +0200

    Ajout des boutons de déconnexion, ajout de liste, et les types dans AccueilView et mise à jour de accueilView.fxml

--- src/main/java/appli/accueil/AccueilView.java
+++ src/main/java/appli/accueil/AccueilView.java
@@ -1,8 +1,10 @@
 package appli.accueil;
 
+import appli.StartApplication;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 
 public class AccueilView {
 
@@ -13,7 +15,39 @@ public class AccueilView {
     private Button retourButton;
 
     @FXML
+    private Button deconnexionButton;
+
+    @FXML
+    private Button ajouterListeButton;{
+        System.out.println("Ajouter une liste");
+    }
+    @FXML
+    private Button lesTypesButton;{
+        System.out.println("Les types");
+    }
+
+    @FXML
+    private TableView<Liste> tableauListe;{
+        System.out.println("Tableau de listes");
+    }
+
+    @FXML
     private void handleRetour() {
         System.out.println("Retour à l'accueil");
+    }
+
+    @FXML
+    private void handleDeconnexion() {
+        StartApplication.changeScene("accueil/loginView", "Login");
+    }
+
+    @FXML
+    private void handleAjouterListe() {
+        // Handle adding a list
+    }
+
+    @FXML
+    private void handleLesTypes() {
+        // Handle showing types
     }
 }
